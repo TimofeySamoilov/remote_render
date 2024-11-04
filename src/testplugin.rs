13fce0d455c83d9064e18f74230a55fc268178e1
TimofeiSamoilov <tssamoilov@edu.hse.ru>
2024-11-04 21:57:50 +0300

    send теперь имеет task

--- src/testplugin.rs
+++ src/testplugin.rs
@@ -1,24 +1,34 @@
 use bevy::{prelude::*, transform::commands};
-pub struct MyPlugin;
+use tokio::runtime::Runtime;
+pub struct MyPlugin { runtime: Option<Runtime>,}
 impl MyPlugin {
-    fn init(&self) {
-        println!("Hello from plugin!");
+    pub fn init(&mut self) {
+        println!("Init from plugin!");
+        self.runtime = Some(Runtime::new().expect("Runtime creation fail!"));
     }
-    fn send(&self) {
-        println!("hello from send!");
+    pub fn send(&self) {
+        if let Some(runtime) = &self.runtime {
+            runtime.spawn(async {
+                while (1 == 1) {
+                    println!("hiii");
+                }
+            });
+        }
     }
 }
 
 impl Plugin for MyPlugin {
     fn build(&self, app: &mut App) {
-        self.init();
-        self.send();
+        
     }
 }
 
-
 fn main() {
+    let mut async_plugin = MyPlugin { runtime: None };
+    async_plugin.init();
+    async_plugin.send();
+    
     std::env::set_var("WGPU_BACKEND", "vulkan");
 
-    App::new().add_plugins(MyPlugin).run();
+    App::new().add_plugins(async_plugin).run();
 }
