0f2ec598027e36f09b7279b51f5ddb8b04db3a1b
TimofeiSamoilov <tssamoilov@edu.hse.ru>
2024-12-24 14:31:32 +0300

    update

--- src/client.rs
+++ src/client.rs
@@ -1,96 +1,149 @@
+use eframe::egui::{self, TextureOptions};
+use rdev::{listen, EventType};
 use remote_render::greeter_client::GreeterClient;
 use remote_render::HelloRequest;
-use tokio_stream::{StreamExt};
-use tonic::transport::Channel;
 use std::sync::{Arc, Mutex};
-use rdev::{listen, EventType};
-use eframe::egui::{self, TextureOptions};
 use tokio::sync::mpsc;
+use tokio_stream::StreamExt;
+use tonic::transport::Channel;
 
-pub mod remote_render { tonic::include_proto!("remote_render"); }
+pub mod remote_render {
+    tonic::include_proto!("remote_render");
+}
 
-struct ScreenApp {screen_length: usize, screen_height: usize, pixels: Vec<u8>, receiver: mpsc::Receiver<Vec<u8>>}
+struct ScreenApp {
+    screen_length: usize,
+    screen_height: usize,
+    pixels: Vec<u8>,
+    receiver: mpsc::Receiver<Vec<u8>>,
+    stop_sender: mpsc::Sender<bool>,
+}
 
 #[tokio::main]
 async fn main() -> eframe::Result<()> {
     eframe::run_native(
         "App",
         eframe::NativeOptions::default(),
-        Box::new(|cc| {
-            let app = ScreenApp::default(); 
+        Box::new(|_cc| {
+            let app = ScreenApp::default();
             Ok(Box::new(app) as Box<dyn eframe::App>)
         }),
     )
 }
 
-
-// default() has an async fn, which is connecting to server 
+// default() has an async fn, which is connecting to server
 impl Default for ScreenApp {
     fn default() -> Self {
-        let shared_pixels: Vec<u8> = vec![0; 500 * 500 *4];
+        let shared_pixels: Vec<u8> = vec![0; 500 * 500 * 4];
         let (tx, rx): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) = mpsc::channel(1);
-        let shared_tx = tx.clone(); // Clone sender for use in the async block
+        let shared_tx = tx.clone(); // clone sender for use in the async block
+        let (stop_tx, stop_rx): (mpsc::Sender<bool>, mpsc::Receiver<bool>) = mpsc::channel(1);
+        let stop_rx = Arc::new(Mutex::new(stop_rx));
+        let stop_rx_clone = Arc::clone(&stop_rx);
         tokio::spawn(async move {
             match GreeterClient::connect("http://[::1]:50051").await {
-               Ok(mut client) => {
-                   let pressed_key = Arc::new(Mutex::new(String::from("")));
-                   let pressed_key_clone = Arc::clone(&pressed_key);
-                   let _keyboard_check = tokio::spawn(keyboard(pressed_key));
-                   let _communication = streaming_data(&mut client, 10000000, pressed_key_clone, shared_tx).await;
-                 
-                   Ok::<(), crate::egui::Key>(())
-               },
-               Err(e) => {
-                   eprintln!("Error connecting to server: {}", e);
-                  Err(crate::egui::Key::A) // return a default error
-               }
-           }
-       });
-        ScreenApp {screen_length: 500, screen_height: 500, pixels: shared_pixels, receiver: rx}
+                Ok(mut client) => {
+                    let pressed_key = Arc::new(Mutex::new(String::from("")));
+                    let pressed_key_clone = Arc::clone(&pressed_key);
+
+                    let _keyboard_check = tokio::spawn(keyboard(pressed_key, stop_rx));
+                    let _communication =
+                        streaming_data(&mut client, 100000000, pressed_key_clone, shared_tx, stop_rx_clone).await;
+
+                    Ok::<(), crate::egui::Key>(())
+                }
+                Err(e) => {
+                    eprintln!("Error connecting to server: {}", e);
+                    Err(crate::egui::Key::A) // return a default error
+                }
+            }
+        });
+        // making ScreenApp
+        ScreenApp {
+            screen_length: 500,
+            screen_height: 500,
+            pixels: shared_pixels,
+            receiver: rx,
+            stop_sender: stop_tx,
+        }
     }
-}  
+}
 
 impl eframe::App for ScreenApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("SuperWindow");
+            // copy data from receiver channel
             while let Ok(data) = self.receiver.try_recv() {
                 self.pixels.copy_from_slice(&data);
             }
-            // Создание текстуры из пикселей
-            let image = egui::ColorImage::from_rgba_unmultiplied([self.screen_length, self.screen_height], &self.pixels);
+            // making texture from pixels
+            let image = egui::ColorImage::from_rgba_unmultiplied(
+                [self.screen_length, self.screen_height],
+                &self.pixels,
+            );
             let texture = ctx.load_texture("my_texture", image, TextureOptions::default());
             ui.image(&texture);
             ctx.request_repaint();
         });
-    }  
-}
-
-//Client's part
-async fn streaming_data(client: &mut GreeterClient<Channel>, num: usize, pressed_key: Arc<Mutex<String>>, sender: mpsc::Sender<Vec<u8>>) {
-    let stream = client.say_hello( HelloRequest { message: "client connected".to_string()}).await.unwrap().into_inner();
-    let mut stream = stream.take(num);
-    while let Some(item) = stream.next().await {
-        let _ = sender.send(item.unwrap().message.to_vec()).await;
-        client.say_hello( HelloRequest { message: format!("{:?}", pressed_key.lock()).to_string()}).await.unwrap().into_inner();
+    }
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
+        let _ = self.stop_sender.try_send(true);
     }
 }
 
-//Function are monitoring keyboard buttons
-async fn keyboard(shared_string: Arc<Mutex<String>>) {
-    listen (move |event| {
+// client's part
+async fn streaming_data(
+    client: &mut GreeterClient<Channel>,
+    num: usize,
+    pressed_key: Arc<Mutex<String>>,
+    sender: mpsc::Sender<Vec<u8>>,
+    stop_rx: Arc<Mutex<mpsc::Receiver<bool>>>,
+) {
+    let stream = client
+        .say_hello(HelloRequest {
+            message: "client connected".to_string(),
+        })
+        .await
+        .unwrap()
+        .into_inner();
+    let mut stream = stream.take(num);
+    while let Some(item) = stream.next().await {
+        let _ = sender.send(item.unwrap().message.to_vec()).await;
+        client.say_hello(HelloRequest {
+                message: format!("{:?}", pressed_key.lock()).to_string(),
+            })
+            .await
+            .unwrap()
+            .into_inner();
+    }
+}
+
+// function monitoring keyboard buttons
+async fn keyboard(shared_string: Arc<Mutex<String>>, stop_rx: Arc<Mutex<mpsc::Receiver<bool>>>) {
+    listen(move |event| {
+        
+        while let Ok(data) = stop_rx.lock().unwrap().try_recv() {
+            println!("Keyboard is closed");
+            return;
+        }
+
         match event.event_type {
             EventType::KeyPress(key) => {
-                //println!("Key is pressed: {:?}", key);
                 let mut s = shared_string.lock().unwrap();
                 *s = format!("{:?}", key);
-                if key == rdev::Key::Escape {
+                
+                //println!("{s}");
+                /*if key == rdev::Key::Escape {
                     println!("Process is over");
                     std::process::exit(0);
-                }
+                }*/
             }
-            _ => { let mut s = shared_string.lock().unwrap();
-            *s = "No one is pressed".to_string(); }
+            _ => {
+                let mut s = shared_string.lock().unwrap();
+                *s = "No one is pressed".to_string();
+            }
         }
-    }).unwrap();
+    })
+    .unwrap();
 }
--- src/server_plugin.rs
+++ src/server_plugin.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
+use bevy_tokio_tasks::*;
 use remote_render::greeter_server::{Greeter, GreeterServer};
 use remote_render::{HelloReply, HelloRequest};
 use std::{error::Error, pin::Pin, time::Duration};
 use tokio::sync::mpsc;
 use tokio_stream::{wrappers::ReceiverStream, Stream};
 use tonic::{transport::Server, Request, Response, Status};
-use bevy_tokio_tasks::*;
+
+// this is bevy plugin with grpc-server
+// uses special plugin: bevy_tokio_tasks
+// which can allow using (tokio async + bevy app)
 
 pub mod remote_render {
     tonic::include_proto!("remote_render");
@@ -17,19 +21,26 @@ pub struct MyGreeter {}
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
     type SayHelloStream = Pin<Box<dyn Stream<Item = Result<HelloReply, Status>> + Send>>;
-    
-    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<Self::SayHelloStream>, Status> {
+
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<Self::SayHelloStream>, Status> {
         println!("{:?}", request.into_inner().message);
 
         let (tx, rx) = mpsc::channel(128);
-        
+
         tokio::spawn(async move {
             let mut a: u8 = 0;
             let mut screen = vec![0; 500 * 500 * 4];
-            while let Ok(_) = tx.send(Result::<_, Status>::Ok(HelloReply {
-                message: screen.clone(),
-            })).await {
-                tokio::time::sleep(Duration::from_millis(128)).await;
+            // communication with client
+            while let Ok(_) = tx
+                .send(Result::<_, Status>::Ok(HelloReply {
+                    message: screen.clone(),
+                }))
+                .await
+            {
+                tokio::time::sleep(Duration::from_millis(64)).await;
                 a = (a + 1) % 3;
                 let save = set_color(a);
                 for x in 0..500 {
@@ -47,11 +58,13 @@ impl Greeter for MyGreeter {
         });
 
         let output_stream = ReceiverStream::new(rx);
-        Ok(Response::new(Box::pin(output_stream) as Self::SayHelloStream))
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::SayHelloStream
+        ))
     }
 }
 
-
+// function, which starts sever
 async fn start_server() -> Result<(), Box<dyn Error>> {
     let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter {};
@@ -62,12 +75,16 @@ async fn start_server() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-
+// system, which uses start_server()
 fn server_system(runtime: ResMut<TokioTasksRuntime>) {
     runtime.spawn_background_task(|_ctx| async move {
         match start_server().await {
-            Ok(_) => {println!("OKKK");},
-            Err(e) => {eprintln!("{:?}", e);}
+            Ok(_) => {
+                println!("Server started...");
+            }
+            Err(e) => {
+                eprintln!("Error with starting server: {:?}", e);
+            }
         }
     });
 }
@@ -81,6 +98,7 @@ fn main() {
         .run();
 }
 
+// this function return different colors
 fn set_color(a: u8) -> (u8, u8, u8) {
     match a {
         0 => (255, 0, 0),
