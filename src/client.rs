727cdf6c2b54788aa8db08d21bda549f99f9baf9
TimofeiSamoilov <tssamoilov@edu.hse.ru>
2025-01-15 21:25:01 +0300

    updated remout render system

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,12 @@
 use eframe::egui::{self, TextureOptions};
 use rdev::{listen, EventType};
 use remote_render::greeter_client::GreeterClient;
-use remote_render::HelloRequest;
-use std::sync::{Arc, Mutex};
+use remote_render::key_board_control_client::KeyBoardControlClient;
+use remote_render::ControlRequest;
+use std::{
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tonic::transport::Channel;
@@ -35,7 +39,7 @@ async fn main() -> eframe::Result<()> {
 impl Default for ScreenApp {
     fn default() -> Self {
         let shared_pixels: Vec<u8> = vec![0; 900 * 900 * 4];
-        let (tx, rx): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) = mpsc::channel(1);
+        let (tx, rx): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) = mpsc::channel(10000);
         let shared_tx = tx.clone(); // clone sender for use in the async block
         let (stop_tx, stop_rx): (mpsc::Sender<bool>, mpsc::Receiver<bool>) = mpsc::channel(1);
         let stop_rx = Arc::new(Mutex::new(stop_rx));
@@ -43,24 +47,44 @@ impl Default for ScreenApp {
         tokio::spawn(async move {
             match GreeterClient::connect("http://[::1]:50051").await {
                 Ok(mut client) => {
-                    let pressed_key = Arc::new(Mutex::new(String::from("")));
-                    let pressed_key_clone = Arc::clone(&pressed_key);
-
-                    let _keyboard_check = tokio::spawn(keyboard(pressed_key, stop_rx));
-                    let _communication = streaming_data(
-                        &mut client,
-                        100000000,
-                        pressed_key_clone,
-                        shared_tx,
-                        stop_rx_clone,
-                    )
-                    .await;
+                    let _communication =
+                        streaming_data(&mut client, shared_tx, stop_rx_clone).await;
 
                     Ok::<(), crate::egui::Key>(())
                 }
                 Err(e) => {
                     eprintln!("Error connecting to server: {}", e);
                     Err(crate::egui::Key::A) // return a default error
+                }
+            }
+        });
+
+        tokio::spawn(async move {
+            match KeyBoardControlClient::connect("http://[::1]:50052").await {
+                Ok(mut client) => {
+                    let pressed_key = Arc::new(Mutex::new(String::from("")));
+                    let pressed_key_clone = Arc::clone(&pressed_key);
+                    let _keyboard_check = tokio::spawn(keyboard(pressed_key, stop_rx));
+                    loop {
+                        let key_clone = pressed_key_clone.lock().unwrap().to_string();
+
+                        match client
+                            .say_keyboard(ControlRequest {
+                                message: key_clone.clone(),
+                            })
+                            .await
+                        {
+                            Ok(_) => {}
+                            Err(e) => {
+                                println!("Error sending key: {}", e)
+                            }
+                        }
+
+                        tokio::time::sleep(Duration::from_millis(8)).await;
+                    }
+                }
+                Err(e) => {
+                    println!("Error connecting to server: {}", e);
                 }
             }
         });
@@ -101,35 +125,26 @@ impl eframe::App for ScreenApp {
 // client's part
 async fn streaming_data(
     client: &mut GreeterClient<Channel>,
-    num: usize,
-    pressed_key: Arc<Mutex<String>>,
     sender: mpsc::Sender<Vec<u8>>,
-    stop_rx: Arc<Mutex<mpsc::Receiver<bool>>>,
+    _stop_rx: Arc<Mutex<mpsc::Receiver<bool>>>,
 ) {
-    let stream = client
-        .say_hello(HelloRequest {
+    let mut stream = client
+        .say_hello(ControlRequest {
             message: "client connected".to_string(),
         })
         .await
         .unwrap()
         .into_inner();
-    let mut stream = stream.take(num);
+
     while let Some(item) = stream.next().await {
         let _ = sender.send(item.unwrap().message.to_vec()).await;
-        client
-            .say_hello(HelloRequest {
-                message: format!("{:?}", pressed_key.lock()).to_string(),
-            })
-            .await
-            .unwrap()
-            .into_inner();
     }
 }
 
 // function monitoring keyboard buttons
 async fn keyboard(shared_string: Arc<Mutex<String>>, stop_rx: Arc<Mutex<mpsc::Receiver<bool>>>) {
     listen(move |event| {
-        while let Ok(data) = stop_rx.lock().unwrap().try_recv() {
+        while let Ok(_data) = stop_rx.lock().unwrap().try_recv() {
             println!("Keyboard is closed");
             return;
         }
--- src/server_plugin.rs
+++ src/server_plugin.rs
@@ -1,18 +1,18 @@
 use bevy::prelude::*;
 use bevy_tokio_tasks::*;
-use chrono::Utc;
 use remote_render::greeter_server::{Greeter, GreeterServer};
-use remote_render::{HelloReply, HelloRequest};
+use remote_render::key_board_control_server::{KeyBoardControl, KeyBoardControlServer};
+use remote_render::{ControlRequest, FrameDispatch};
+use std::sync::{Arc, Mutex};
 use std::{error::Error, pin::Pin, time::Duration};
 use tokio::sync::mpsc;
 use tokio_stream::{wrappers::ReceiverStream, Stream};
 use tonic::{transport::Server, Request, Response, Status};
-use std::sync::{Arc, Mutex};
 
 use bevy::prelude::Circle;
-use bevy::render::{view::screenshot::ScreenshotManager};
+use bevy::render::view::screenshot::ScreenshotManager;
 use bevy::sprite::MaterialMesh2dBundle;
-use bevy::window::{Window, PrimaryWindow, WindowMode};
+use bevy::window::{PrimaryWindow, Window, WindowMode};
 
 // this is bevy render with grpc-server
 // uses special plugin: bevy_tokio_tasks
@@ -22,61 +22,83 @@ use bevy::window::{Window, PrimaryWindow, WindowMode};
 pub mod remote_render {
     tonic::include_proto!("remote_render");
 }
-
 #[derive(Debug)]
-pub struct MyGreeter {
+pub struct RenderService {
     frame_receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>,
+    client_connected: Arc<Mutex<bool>>,
 }
-impl MyGreeter {
-    pub fn new(receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>) -> Self {
-        MyGreeter {
+impl RenderService {
+    pub fn new(
+        receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>,
+        client_connected: Arc<Mutex<bool>>,
+    ) -> Self {
+        RenderService {
             frame_receiver: receiver,
+            client_connected: client_connected,
         }
+    }
+}
+pub struct KeyBoardButtons {}
+impl KeyBoardButtons {
+    pub fn new() -> Self {
+        KeyBoardButtons {}
     }
 }
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    type SayHelloStream = Pin<Box<dyn Stream<Item = Result<HelloReply, Status>> + Send>>;
+impl KeyBoardControl for KeyBoardButtons {
+    async fn say_keyboard(
+        &self,
+        request: Request<ControlRequest>,
+    ) -> Result<Response<ControlRequest>, Status> {
+        let message = request.into_inner().message;
+        println!("Received from client: {:?}", message);
 
+        Ok(Response::new(ControlRequest {
+            message: "OK".to_string(),
+        }))
+    }
+}
+
+#[tonic::async_trait]
+impl Greeter for RenderService {
+    type SayHelloStream = Pin<Box<dyn Stream<Item = Result<FrameDispatch, Status>> + Send>>;
     async fn say_hello(
         &self,
-        request: Request<HelloRequest>,
+        request: Request<ControlRequest>,
     ) -> Result<Response<Self::SayHelloStream>, Status> {
         println!("{:?}", request.into_inner().message);
-
+        let mut client_connected = self.client_connected.lock().unwrap();
+        *client_connected = true;
         let (tx, rx) = mpsc::channel(128);
         let receiver_clone = self.frame_receiver.clone();
         tokio::spawn(async move {
             // communication with client
             loop {
-                let mut screen = vec![0; SCREEN_WIDTH as usize * SCREEN_HEIGHT as usize * 4];
+                let screen: Vec<u8>;
                 let data = receiver_clone.lock().unwrap().try_recv();
-                
+
                 match data {
                     Ok(frame) => {
                         screen = frame;
-                         //println!("{:?} received data length", screen.len());
-                     } ,
+                    }
                     Err(_) => {
-                        //println!("failed to deal with data");
-                        tokio::time::sleep(Duration::from_millis(2)).await; // Try again soon
+                        tokio::time::sleep(Duration::from_millis(1)).await; // Try again soon
                         continue;
-                    },
+                    }
                 }
 
-                if tx
-                    .send(Result::<_, Status>::Ok(HelloReply {
+                match tx
+                    .send(Result::<_, Status>::Ok(FrameDispatch {
                         message: screen.clone(),
                     }))
                     .await
-                    .is_err()
                 {
-                    // Client disconnected
-                    break;
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("frame skipped, {:?}", e);
+                    }
                 }
-
-                // tokio::time::sleep(Duration::from_millis(64)).await;
             }
         });
 
@@ -88,24 +110,39 @@ impl Greeter for MyGreeter {
 }
 
 // function, which starts sever
-async fn start_server(receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>) -> Result<(), Box<dyn Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::new(receiver);
+async fn start_server(
+    receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>,
+    client_connected: Arc<Mutex<bool>>,
+) -> Result<(), Box<dyn Error>> {
+    let greeter_addr = "[::1]:50051".parse()?;
+    let keyboard_addr = "[::1]:50052".parse()?;
+
+    let greeter = RenderService::new(receiver, client_connected);
+    let keyboard = KeyBoardButtons::new();
+
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(GreeterServer::new(greeter))
+            .serve(greeter_addr)
+            .await
+            .expect("Greeter server error");
+    });
+
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
+        .add_service(KeyBoardControlServer::new(keyboard))
+        .serve(keyboard_addr)
         .await?;
+
     Ok(())
 }
 
 // system, which uses start_server()
 fn server_system(runtime: ResMut<TokioTasksRuntime>, communication: ResMut<Communication>) {
-    let mut receiver_clone = communication.receiver.clone();
+    let receiver_clone = communication.receiver.clone();
+    let client_connected = communication.client_connected.clone();
     runtime.spawn_background_task(|_ctx| async move {
-        match start_server(receiver_clone).await {
-            Ok(_) => {
-                println!("Server started...");
-            }
+        match start_server(receiver_clone, client_connected).await {
+            Ok(_) => {}
             Err(e) => {
                 eprintln!("Error with starting server: {:?}", e);
             }
@@ -116,17 +153,50 @@ fn server_system(runtime: ResMut<TokioTasksRuntime>, communication: ResMut<Commu
 fn main() {
     std::env::set_var("WGPU_BACKEND", "vulkan");
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Remote Render".into(),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(bevy_tokio_tasks::TokioTasksPlugin::default())
         .add_systems(Startup, server_system)
         .add_systems(Startup, window_setup)
         .add_systems(Startup, scene_setup)
-        .add_systems(Update, update)
+        .add_systems(Update, scene_update)
         .add_systems(PostUpdate, take_screenshot_system)
         .insert_resource(Communication::default())
         .run();
 }
 
+fn take_screenshot_system(
+    mut screenshot_manager: ResMut<ScreenshotManager>,
+    window_query: Query<(Entity, &Window), With<PrimaryWindow>>,
+    communication: ResMut<Communication>,
+) {
+    let window_entity = window_query.single().0;
+    let sender_clone = communication.sender.clone();
+    let client_connected = communication.client_connected.clone();
+    if *client_connected.lock().unwrap() {
+        match screenshot_manager.take_screenshot(window_entity, move |image: Image| {
+            let pixels = image.data;
+            match sender_clone.try_send(pixels) {
+                Ok(_) => {}
+                Err(e) => {
+                    println!("{:?}", e)
+                }
+            }
+        }) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Failed to create a screenshot: {:?}", e)
+            }
+        }
+    }
+}
+
+// Bevy scene part
 const SCREEN_WIDTH: f32 = 900.0;
 const SCREEN_HEIGHT: f32 = 900.0;
 const BALL_RADIUS: f32 = 50.0;
@@ -138,12 +208,18 @@ struct Ball;
 #[derive(Resource)]
 struct Communication {
     receiver: Arc<Mutex<mpsc::Receiver<Vec<u8>>>>,
-    sender: mpsc::Sender<Vec<u8>>
+    sender: mpsc::Sender<Vec<u8>>,
+    client_connected: Arc<Mutex<bool>>,
 }
 impl Default for Communication {
     fn default() -> Self {
-        let (sender_rx, receiver_rx): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) = mpsc::channel(10000);
-        Communication {receiver: Arc::new(Mutex::new(receiver_rx)), sender: sender_rx}
+        let (sender_rx, receiver_rx): (mpsc::Sender<Vec<u8>>, mpsc::Receiver<Vec<u8>>) =
+            mpsc::channel(10000);
+        Communication {
+            receiver: Arc::new(Mutex::new(receiver_rx)),
+            sender: sender_rx,
+            client_connected: Arc::new(Mutex::new(false)),
+        }
     }
 }
 
@@ -153,20 +229,24 @@ fn window_setup(mut window: Query<&mut Window>) {
     window.mode = WindowMode::Windowed;
 }
 
-fn scene_setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn scene_setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes.add(Circle::new(BALL_RADIUS)).into(),
-        material: materials.add(ColorMaterial::from(Color::WHITE)),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-        ..default()
-    }, Ball));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Circle::new(BALL_RADIUS)).into(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..default()
+        },
+        Ball,
+    ));
 }
 
-fn update(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, With<Ball>>,
-) {
+fn scene_update(time: Res<Time>, mut query: Query<&mut Transform, With<Ball>>) {
     for mut transform in query.iter_mut() {
         let delta_time = time.delta_seconds();
         let move_amount = unsafe { BALL_SPEED } * delta_time;
@@ -175,34 +255,17 @@ fn update(
         transform.translation.x += move_amount;
 
         if transform.translation.x + BALL_RADIUS > SCREEN_WIDTH / 2.0 {
-             transform.translation.x = SCREEN_WIDTH / 2.0 - BALL_RADIUS;
-             unsafe { BALL_SPEED *= -1.0; }
-
+            transform.translation.x = SCREEN_WIDTH / 2.0 - BALL_RADIUS;
+            unsafe {
+                BALL_SPEED *= -1.0;
+            }
         }
 
         if transform.translation.x - BALL_RADIUS < -SCREEN_WIDTH / 2.0 {
             transform.translation.x = -SCREEN_WIDTH / 2.0 + BALL_RADIUS;
-            unsafe { BALL_SPEED *= -1.0; }
+            unsafe {
+                BALL_SPEED *= -1.0;
+            }
         }
-    }
-}
-fn take_screenshot_system(
-    mut screenshot_manager: ResMut<ScreenshotManager>,
-    window_query: Query<(Entity, &Window), With<PrimaryWindow>>,
-    mut communication: ResMut<Communication>,
-    mut query: Query<&mut Transform, With<Ball>>,
-    //runtime: ResMut<TokioTasksRuntime>
-) {
-    let window_entity = window_query.single().0;
-    let sender_clone = communication.sender.clone();
-    match screenshot_manager.take_screenshot(window_entity,  move |image: Image| {
-        let pixels = image.data;
-        match sender_clone.try_send(pixels) {
-            Ok(_) => {},
-            Err(e) => {println!("{:?}", e)}
-        }
-    }) {
-        Ok(_) => {}
-        Err(e) => {println!("Failed to create a screenshot: {:?}", e)}
     }
 }
